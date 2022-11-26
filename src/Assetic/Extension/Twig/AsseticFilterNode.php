467874df3ab08a2ee00e68d89d3488a426e1ecf1
Victor <viktoras@bluedaysolutions.com>
2022-11-26 19:08:41 +0100

    Re-added TwigFormulaLoader

---
+++ src/Assetic/Extension/Twig/AsseticFilterNode.php
@@ -0,0 +1,19 @@
+<?php namespace Assetic\Extension\Twig;
+
+use Twig\Compiler;
+use Twig\Node\Expression\FunctionExpression;
+
+class AsseticFilterNode extends FunctionExpression
+{
+    protected function compileCallable(Compiler $compiler)
+    {
+        $compiler->raw(
+            sprintf(
+                '$this->env->getExtension(\'Assetic\\Extension\\Twig\\AsseticExtension\')->getFilterInvoker(\'%s\')->invoke',
+                $this->getAttribute('name')
+            )
+        );
+
+        $this->compileArguments($compiler);
+    }
+}

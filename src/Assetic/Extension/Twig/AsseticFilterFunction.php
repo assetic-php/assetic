ba880288ea4438b81ae40d6edb9b74c627a0d981
bds-viktoras <35102914+bds-viktoras@users.noreply.github.com>
2022-11-28 02:02:50 +0100

    Re-added TwigFormulaLoader (#37)
    Fixes issue #15 where, when updating to the new version of Twig, a couple of extensions got removed in #4e42ff8 Those extensions are being re-added here together with passing tests.

--- src/Assetic/Extension/Twig/AsseticFilterFunction.php
+++ src/Assetic/Extension/Twig/AsseticFilterFunction.php
@@ -6,6 +6,12 @@ class AsseticFilterFunction
 {
     public static function make(AsseticExtension $extension, $name, $options = [])
     {
+        $options = array_merge($options, [
+            'needs_environment' => false,
+            'needs_context'     => false,
+            'node_class'        => AsseticFilterNode::class,
+        ]);
+
         return new TwigFunction($name, function ($input, array $options) use ($extension, $name) {
             return $extension->getFilterInvoker($name)->invoke($input, $options);
         }, $options);
---
+++ src/Assetic/Extension/Twig/TwigFormulaLoader.php
@@ -0,0 +1,100 @@
+<?php namespace Assetic\Extension\Twig;
+
+use Assetic\Contracts\Factory\Loader\FormulaLoaderInterface;
+use Assetic\Contracts\Factory\Resource\ResourceInterface;
+use Psr\Log\LoggerInterface;
+use Twig\Environment;
+use Twig\Node\Node;
+use Twig\Source;
+
+/**
+ * Loads asset formulae from Twig templates.
+ *
+ * @author Kris Wallsmith <kris.wallsmith@gmail.com>
+ */
+class TwigFormulaLoader implements FormulaLoaderInterface
+{
+    private $twig;
+    private $logger;
+
+    public function __construct(Environment $twig, LoggerInterface $logger = null)
+    {
+        $this->twig = $twig;
+        $this->logger = $logger;
+    }
+
+    public function load(ResourceInterface $resource)
+    {
+        try {
+            $tokens = $this->twig->tokenize(new Source($resource->getContent(), (string) $resource));
+            $nodes  = $this->twig->parse($tokens);
+        } catch (\Exception $e) {
+            if ($this->logger) {
+                $this->logger->error(sprintf('The template "%s" contains an error: %s', $resource, $e->getMessage()));
+            }
+
+            return [];
+        }
+
+        return $this->loadNode($nodes);
+    }
+
+    /**
+     * Loads assets from the supplied node.
+     *
+     * @param Node $node
+     *
+     * @return array An array of asset formulae indexed by name
+     */
+    private function loadNode(Node $node)
+    {
+        $formulae = [];
+
+        if ($node instanceof AsseticNode) {
+            $formulae[$node->getAttribute('name')] = [
+                $node->getAttribute('inputs'),
+                $node->getAttribute('filters'),
+                [
+                    'output'  => $node->getAttribute('asset')->getTargetPath(),
+                    'name'    => $node->getAttribute('name'),
+                    'debug'   => $node->getAttribute('debug'),
+                    'combine' => $node->getAttribute('combine'),
+                    'vars'    => $node->getAttribute('vars'),
+                ],
+            ];
+        } elseif ($node instanceof AsseticFilterNode) {
+            $name = $node->getAttribute('name');
+
+            $arguments = [];
+            foreach ($node->getNode('arguments') as $argument) {
+                $arguments[] = eval('return ' . $this->twig->compile($argument) . ';');
+            }
+
+            $invoker = $this->twig->getExtension(AsseticExtension::class)->getFilterInvoker($name);
+
+            $inputs  = isset($arguments[0]) ? (array) $arguments[0] : [];
+            $filters = $invoker->getFilters();
+            $options = array_replace($invoker->getOptions(), isset($arguments[1]) ? $arguments[1] : []);
+
+            if (!isset($options['name'])) {
+                $options['name'] = $invoker->getFactory()->generateAssetName($inputs, $filters, $options);
+            }
+
+            $formulae[$options['name']] = [$inputs, $filters, $options];
+        }
+
+        foreach ($node as $child) {
+            if ($child instanceof Node) {
+                $formulae += $this->loadNode($child);
+            }
+        }
+
+        if ($node->hasAttribute('embedded_templates')) {
+            foreach ($node->getAttribute('embedded_templates') as $child) {
+                $formulae += $this->loadNode($child);
+            }
+        }
+
+        return $formulae;
+    }
+}

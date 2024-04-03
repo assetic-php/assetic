c448bb491517cdc4e8be4903abad8c92063ae84a
Luke Towers <github@luketowers.ca>
2024-04-03 10:42:55 -0600

    Support TypeScript 5
    https://www.typescriptlang.org/tsconfig#out

--- src/Assetic/Filter/TypeScriptFilter.php
+++ src/Assetic/Filter/TypeScriptFilter.php
@@ -35,7 +35,7 @@ class TypeScriptFilter extends BaseNodeFilter
     {
         $args = [
             '{INPUT}',
-            '--out',
+            '--outFile',
             '{OUTPUT}'
         ];
 

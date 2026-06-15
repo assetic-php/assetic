73f2b22c8d9950c2338a912f0bfd2e73df71dee0
Ben Thomson <git@alfreido.com>
2026-06-15 07:40:38 +0000

    Support both Tailwind v3 and v4 in filter

--- src/Assetic/Filter/TailwindCssFilter.php
+++ src/Assetic/Filter/TailwindCssFilter.php
@@ -34,6 +34,11 @@ class TailwindCssFilter extends BaseProcessFilter
     protected $autoprefix = true;
 
     /**
+     * @var int|null The installed major version of the Tailwind CSS CLI utility, as an integer (eg. 3 for v3.*, 4 for v4.*). Cached after the first call to getVersion().
+     */
+    protected static $cliVersion = null;
+
+    /**
      * Sets the path for the configuration file.
      */
     public function setConfigPath(string $configPath): void
@@ -62,27 +67,66 @@ class TailwindCssFilter extends BaseProcessFilter
      */
     public function filterLoad(AssetInterface $asset)
     {
-        $args = [
-            '--input',
-            '{INPUT}',
-            '--output',
-            '{OUTPUT}'
-        ];
+        $args = ['--input', '{INPUT}', '--output', '{OUTPUT}'];
+
+        $content = $asset->getContent();
 
         if (!is_null($this->configPath)) {
-            $args[] = '--config';
-            $args[] = $this->configPath;
+            if ($this->getVersion() === 4) {
+                $content = '@config "' . $this->configPath . '";' . PHP_EOL . $content;
+            } else {
+                $args[] = '--config';
+                $args[] = $this->configPath;
+            }
         }
 
         if ($this->minify) {
             $args[] = '--minify';
         }
 
-        if (!$this->autoprefix) {
+        if ($this->getVersion() === 4) {
+            $args[] = '--cwd';
+            $args[] = $this->workingDirectory ?? (!is_null($this->configPath) ? dirname($this->configPath) : null);
+        }
+
+        if ($this->getVersion() === 3 && !$this->autoprefix) {
             $args[] = '--no-autoprefixer';
         }
 
-        $result = $this->runProcess($asset->getContent(), $args);
+        $result = $this->runProcess($content, $args);
         $asset->setContent($result);
+    }
+
+    /**
+     * Gets the installed version of the Tailwind CSS CLI utility.
+     */
+    protected function getVersion(): int
+    {
+        // We don't expect the binary version to change mid-execution
+        if (!is_null(self::$cliVersion)) {
+            return self::$cliVersion;
+        }
+
+        $process = $this->createProcess(array_merge($this->getPathArgs(), ['--help']));
+        $code = $process->run();
+
+        if ($code !== 0) {
+            throw new \RuntimeException(
+                sprintf(
+                    'Failed to execute "%s --help" to determine Tailwind CSS CLI version: %s',
+                    $this->binaryPath,
+                    $process->getErrorOutput(),
+                ),
+            );
+        }
+
+        $output = trim($process->getOutput());
+        if (!preg_match('/^[^\n\r]*tailwindcss +v([0-9])\./i', $output, $matches) || !isset($matches[1])) {
+            throw new \RuntimeException('Unable to find version identifier in Tailwind CSS CLI utility');
+        }
+
+        self::$cliVersion = (int) $matches[1] === 4 ? 4 : 3;
+
+        return self::$cliVersion;
     }
 }
